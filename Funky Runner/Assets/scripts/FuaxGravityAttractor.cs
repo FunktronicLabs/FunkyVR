c6d59e61b445d48c05f3cd1d30c4a1a1030802f8
jjthep1nkranger <jimmyannes98@gmail.com>
2015-07-08 11:27:34 -0700

    adding planets and stuff

---
+++ Funky Runner/Assets/scripts/FuaxGravityAttractor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fauxgravityattractor : MonoBehaviour {
+
+	public fauxgravityattractor attractor;
+
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.constraints = Rigidbody.c
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		attractor.Attract(myTransform);
+	}
+}
