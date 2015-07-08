9f4f4fb88a1cde4d6146f93f73a3fc45baa59a89
Eddie Lee <eddie@funktroniclabs.com>
2015-07-08 15:01:04 -0700

    I CAN FLY!
    Not really...

---
+++ test of grav theory/Assets/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float moveSpeed = 15;
+	private Vector3 moveDirection;
+
+
+	void Update() {
+		moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical")).normalized;
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+	}
+}
---
+++ test of grav theory/Assets/findCenter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class findCenter : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime = 0.3F;
+	private Vector3 currentVelocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = target.position;
+		transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+	}
+}
